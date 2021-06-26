cea1742a1f68a4105d5bd0afc5cf6beb2ff363f8
Joseph Burke <joseph_burke@outlook.com>
2021-06-26 13:58:57 +0100

    Add dev + lint scripts. Refactor --> ES6

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('index');
+router.get('/', (req, res) => {
+  res.send('hddwd');
 });
 
-module.exports = router;
+export default router;
