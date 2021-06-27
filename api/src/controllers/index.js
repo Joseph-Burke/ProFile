ed7c0e95d8aa431587389abf3908c2fdb942490a
Joseph Burke <joseph_burke@outlook.com>
2021-06-27 11:25:16 +0100

    Rename Controller.js

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,4 +1,4 @@
-import { Controller } from './Controller';
+import { Controller } from './controller';
 
 export const usersController = new Controller('user');
 export const companiesController = new Controller('company');
